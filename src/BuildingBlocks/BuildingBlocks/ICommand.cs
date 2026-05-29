5fb003adb65fbb747260429be5288dee1a565d07
m.ferdosiyan@remote.pc <m.ferdosiyan@remote.pc>
2026-05-30 01:47:51 +0330

    Create Product feature and BuildingBlocks

---
+++ src/BuildingBlocks/BuildingBlocks/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+
+namespace BuildingBlocks
+{
+	public interface ICommand : ICommand<Unit> 
+	{
+	}
+	public interface ICommand<out TResponse> : IRequest<TResponse>
+	{
+	}
+}
