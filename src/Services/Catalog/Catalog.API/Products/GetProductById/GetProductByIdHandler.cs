eb832e1b39c662dba54479b6310c78c16aebce7e
m.ferdosiyan@local.pc <m.ferdosiyan@local.pc>
2026-06-06 19:11:17 +0330

    create get product by id handler

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,23 @@
+﻿using BuildingBlocks;
+
+namespace Catalog.API.Products.GetProductById
+{
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(Product product);
+    public class GetProductByIdQueryHandler(IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger) 
+        : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Query}", query);
+
+            var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+            // as public record GetProductByIdResult(Product product); contain just a record of product, it is not required to use ToList()
+            if (product is null)
+            {
+                throw new ProductNotFoundExcception();
+            }
+            return new GetProductByIdResult(product);
+        }
+    }
+}
