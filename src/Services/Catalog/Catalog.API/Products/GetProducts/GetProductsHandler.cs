382ed028de122f0c46c52d41be3182317a3cb89c
m.ferdosiyan@local.pc <m.ferdosiyan@local.pc>
2026-06-06 18:07:19 +0330

    add logger in GetProdutcsQueryHandler

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -7,10 +7,11 @@ namespace Catalog.API.Products.GetPRoducts
     public record GetProductQuery() : IQuery<GetProductsResult>;
     public record GetProductsResult(IEnumerable<Product> Products);
 
-    internal class GetProductsQueryHandler(IDocumentSession session) : IQueryHandler<GetProductQuery, GetProductsResult>
+    internal class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger) : IQueryHandler<GetProductQuery, GetProductsResult>
     {
         public async Task<GetProductsResult> Handle(GetProductQuery query, CancellationToken cancellationToken)
         {
+            logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
             var products = await session.Query<Product>().ToListAsync(cancellationToken);
             return new GetProductsResult(products);
         }
