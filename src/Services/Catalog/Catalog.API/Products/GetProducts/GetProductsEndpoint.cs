5ae1576494112ffb3678a68c8459d9bd193d3e8d
m.ferdosiyan@local.pc <m.ferdosiyan@local.pc>
2026-06-06 17:56:52 +0330

    create get products endpoint

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -1,6 +1,27 @@
-﻿namespace Catalog.API.Products.GetPRoducts
+﻿using Catalog.API.Products.CreateProduct;
+
+namespace Catalog.API.Products.GetPRoducts
 {
-    public class GetProductsEndpoint
+    public record GetProductRequest();
+    public record GetProductsResponse(Product products);
+    public class GetProductsEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async (ISender sender) => {
+                // use mediatR to send query to GetProductsquery
+                var result = await sender.Send(new GetProductQuery());
+                
+                // map result of query handler to GetProductsResponse
+                var response = result.Adapt<GetProductsResponse>();
+
+                return Results.Ok(response);
+            })
+                .WithName("GetProduct")
+                .Produces<GetProductsResponse>(StatusCodes.Status201Created)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Get Poduct")
+                .WithDescription("Get Poduct");
+        }
     }
 }
